c4795167b977708d520fa5ab42e9a255af986372
astyrisk <marguesto@disroot.org>
2024-05-02 00:59:47 +0200

    Init: Photo Section

---
+++ src/routes/video/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { Navbar, NavBrand, NavLi, NavUl } from 'flowbite-svelte';
+</script>
+
+
+<main>
+    <Navbar  >
+        <NavBrand href="/">
+            <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">&FILE</span>
+        </NavBrand>
+        <NavUl >
+            <NavLi href="/photo">Photo</NavLi>
+            <NavLi href="/audio">Audio</NavLi>
+            <NavLi href="/video">Video</NavLi>
+            <NavLi href="/text">Text</NavLi>
+            <NavLi href="/">Profile</NavLi>
+            <NavLi href="/register" >Logout</NavLi>
+        </NavUl>
+    </Navbar>
+</main>
+
+<style>
+	main{
+			max-width: 70%;
+			margin: 3em auto;
+	}
+</style>
